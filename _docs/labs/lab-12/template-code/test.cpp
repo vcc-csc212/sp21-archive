24889f54dbba00af268478a5fada64c8393afc97
Christian Esteves <c.esteves92@gmail.com>
2021-04-13 22:06:48 -0400

    Fixed test cases

--- _docs/labs/lab-12/template-code/test.cpp
+++ _docs/labs/lab-12/template-code/test.cpp
@@ -6,10 +6,15 @@
 TEST_CASE("Add Edge") {
     Graph g;
 
-    g.addEdge(0, 1, 3);
-    g.addEdge(0, 2, 3);
-    g.addEdge(1, 3, 2);
-    g.addEdge(2, 3, 1);
+    g.addEdge(0, 1, 7);
+    g.addEdge(0, 2, 9);
+    g.addEdge(0, 5, 14);
+    g.addEdge(1, 2, 10);
+    g.addEdge(1, 3, 15);
+    g.addEdge(2, 3, 11);
+    g.addEdge(2, 5, 2);
+    g.addEdge(3, 4, 6);
+    g.addEdge(4, 5, 9);
 
     g.displayGraph();
 }
@@ -19,53 +24,73 @@ TEST_CASE("DFS") {
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.DFS(0, oss);
 
-        REQUIRE(oss.str() == "0 1 3 2 ");
+        REQUIRE(oss.str() == "0 1 2 3 4 5 ");
     }
     SUBCASE("From 1"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.DFS(1, oss);
 
-        REQUIRE(oss.str() == "1 3 ");
+        REQUIRE(oss.str() == "1 0 2 3 4 5 ");
     }
     SUBCASE("From 2"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.DFS(2, oss);
 
-        REQUIRE(oss.str() == "2 3 ");
+        REQUIRE(oss.str() == "2 0 1 3 4 5 ");
     }
     SUBCASE("From 3"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.DFS(3, oss);
 
-        REQUIRE(oss.str() == "3 ");
+        REQUIRE(oss.str() == "3 1 0 2 5 4 ");
     }
 }
 
@@ -74,48 +99,68 @@ TEST_CASE("BFS") {
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.BFS(0, oss);
-        REQUIRE(oss.str() == "0 1 2 3 ");
+        REQUIRE(oss.str() == "0 1 2 5 3 4 ");
     }
     SUBCASE("From 1"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.BFS(1, oss);
-        REQUIRE(oss.str() == "1 3 ");
+        REQUIRE(oss.str() == "1 0 2 3 5 4 ");
     }
     SUBCASE("From 2"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.BFS(2, oss);
-        REQUIRE(oss.str() == "2 3 ");
+        REQUIRE(oss.str() == "2 0 1 3 5 4 ");
     }
     SUBCASE("From 3"){
         Graph g;
         std::ostringstream oss;
 
-        g.addEdge(0, 1, 3);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 2);
-        g.addEdge(2, 3, 1);
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        g.addEdge(3, 4, 6);
+        g.addEdge(4, 5, 9);
 
         g.BFS(3, oss);
-        REQUIRE(oss.str() == "3 ");
+        REQUIRE(oss.str() == "3 1 2 4 0 5 ");
     }
 }
